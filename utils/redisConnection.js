fa74822fb428ce34a8fd21bd4280a876621ff3b5
Hamza El Massoudi <hmz.elm@outlook.sa>
2023-12-17 15:13:09 +0100

    Edit : Update Redis connection configuration

--- utils/redisConnection.js
+++ utils/redisConnection.js
@@ -1,6 +1,14 @@
-const redis = require('redis');
+const { createClient } = require('redis');
 
-const redisClient = redis.createClient();
+require('dotenv').config();
+
+const redisClient = createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+});
 
 redisClient.on('connect', () => {
   console.log('Redis client connected');
